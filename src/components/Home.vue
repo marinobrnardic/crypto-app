7693342d89ad877cd3784287054a6734c2899d4e
Marino Brnardic <mbrnardic@coria.com>
2020-10-31 01:31:08 +0100

    Added DisplayCurrency.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,52 @@
 <template>
-    <div>Home</div>
+  <div class="home">
+		<h1 class="home--title">Top 10 cryptocurrencies</h1>
+		<table class="home--table">
+			<thead class="home--table-header">
+				<tr>
+					<th>Name</th>
+					<th>Price</th>
+					<th>Market Cap</th>
+					<th>Volume</th>
+				</tr>
+			</thead>
+			<tbody class="home--table-body">
+				<tr>
+					<td>Bitcoin</td>
+					<td>100</td>
+					<td>200</td>
+					<td>300</td>
+				</tr>
+				<tr>
+					<td>Ethereum</td>
+					<td>100</td>
+					<td>200</td>
+					<td>300</td>
+				</tr>
+				<tr>
+					<td>Cardano</td>
+					<td>100</td>
+					<td>200</td>
+					<td>300</td>
+				</tr>
+			</tbody>
+		</table>
+  </div>
 </template>
 
-<style lang="sass">
+<script>
+export default {
 
+};
+</script>
+
+<style lang="sass">
+.home
+	width: 90%
+	margin: 0 auto	
+.home--table
+	width: 100%
+.home--table-header
+	text-align: left
+	border-bottom: 1px solid red
 </style>
