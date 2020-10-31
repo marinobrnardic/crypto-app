414d5863a82f658b939bdcc06ee72d234a344ec8
Marino Brnardic <mbrnardic@coria.com>
2020-10-31 17:53:18 +0100

    Basic styling on Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,24 +1,28 @@
 <template>
   <div class="home">
-		<h1 class="home--title">Top 10 cryptocurrencies</h1>
-		<table class="home--table">
-			<thead class="home--table-header">
-				<tr>
-					<th>Name</th>
-					<th>Price</th>
-					<th>Market Cap</th>
-					<th>Volume</th>
-				</tr>
-			</thead>
-			<tbody class="home--table-body">
-				<tr v-for="crypto in cryptos" :key="crypto.id">
-					<td>{{ crypto.name }}</td>
-					<td>{{ crypto.current_price | currency('USD', 'en-US') }}</td>
-					<td>{{ crypto.market_cap | currency('USD', 'en-US') }}</td>
-					<td>{{crypto.total_volume | currency('USD', 'en-US')}}</td>
-				</tr>
-			</tbody>
-		</table>
+		<h1 class="home--title">Top 10 Cryptocurrencies</h1>
+			<!-- <table class="home--table">
+				<thead class="home--table-header">
+					<tr>
+						<th class="home--table-heading"></th>
+						<th class="home--table-heading">Name</th>
+						<th class="home--table-heading">Price</th>
+						<th class="home--table-heading">Change 24h</th>
+						<th class="home--table-heading">Market Cap</th>
+						<th class="home--table-heading" align="right">Volume</th>
+					</tr>
+				</thead>
+				<tbody class="home--table-body">
+					<tr v-for="crypto in cryptos" :key="crypto.id">
+						<td><img class="home--table-icon" :src="crypto.image" alt=""></td>
+						<td><router-link :to="{name: 'DisplayCurrency', params: {id: crypto.symbol}}" class="home--table-link">{{ crypto.name }}</router-link></td>
+						<td>{{ crypto.current_price | currency('USD', 'en-US') }}</td>
+						<td>{{ crypto.price_change_percentage_24h }}</td>
+						<td>{{ crypto.market_cap | currency('USD', 'en-US') }}</td>
+						<td align="right">{{ crypto.total_volume | currency('USD', 'en-US')}}</td>
+					</tr>
+				</tbody>
+			</table> -->
   </div>
 </template>
 
@@ -38,11 +42,23 @@ export default {
 
 .home
 	font-family: $font-oswald
-	width: 90%
+	max-width: 1400px
 	margin: 0 auto	
 .home--table
 	width: 100%
+	cellpadding: 0
+	cellspacing: 0
 .home--table-header
 	text-align: left
 	border-bottom: 1px solid red
+	font-size: 14px
+	color: $dark-grey
+.home--table-icon
+	max-width: 2rem
+
+.home--table-link
+	color: $blue
+	text-decoration: none
+	&:hover
+		color: darken($blue, 10%)
 </style>
